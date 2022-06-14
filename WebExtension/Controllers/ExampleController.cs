1acc9be56a6a209609113d4c84e4d74da79aeb78
Praveen Madhav <praveen@directscale.com>
2022-06-14 16:18:12 +0530

    Example API

---
+++ WebExtension/Controllers/ExampleController.cs
@@ -0,0 +1,21 @@
+﻿using DirectScale.Disco.Extension.Middleware;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebExtension.Controllers
+{
+    [ExtensionAuthorize]
+    [Route("api/example")]
+    [ApiController]
+    public class ExampleController : ControllerBase
+    {
+        [HttpPost("testEndpoint")]
+        public async Task<ActionResult> TestEndpoint()
+        {
+          return await Task.Run(() => (Ok("Hello World!")));
+        }
+    }
+}
